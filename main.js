250ccd1cc892c645807e96a17a3454069dd12043
tashadezelsky <77847692+tashadezelsky@users.noreply.github.com>
2021-04-02 20:14:19 +0900

    fixing js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const cursor = document.querySelector(".cursor");
 window.addEventListener("mousemove", function(e){
-cursor.style.left= (e.x-100)+"px";
-cursor.style.top= (e.y-100)+"px";
+  cursor.style.left = (e.x-100)+"px";
+  cursor.style.top  = (e.y-100)+"px";
+  
 })
