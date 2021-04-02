9a52d3217619f482c8ab15727ab4c7c6252caff3
tashadezelsky <77847692+tashadezelsky@users.noreply.github.com>
2021-04-02 19:55:45 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+const cursor = document.querySelector(".cursor");
+window.addEventListener("mousemove", function(e){
+cursor.style.left= (e.x-100)+"px";
+cursor.style.top= (e.y-100)+"px";
+})
+</script>
